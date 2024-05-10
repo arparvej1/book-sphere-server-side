b38877c4c20fe8967e639db3e5d9d50a62148998
arparvej1 <parvej8pro@gmail.com>
2024-05-10 16:52:18 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.options("", cors(corsConfig))
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.MONGODB_TEN_USER}:${process.env.MONGODB_TEN_PASS}@cluster0.esbrpdb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.MONGODB_ELEVEN_USER}:${process.env.MONGODB_ELEVEN_PASS}@cluster0.esbrpdb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,6 +28,36 @@ const client = new MongoClient(uri, {
   }
 });
 
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    const userCollection = client.db('bookSphereDB').collection('users');
+
+    // --- send user
+    app.get('/users', async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // --- received user from client
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
+
+// --- run server
 app.get('/',  (req, res) => {
   res.send('Server is running...')
 });
