2c03c9ea3df1669dec437c5b3b4c9325139f2c31
arparvej1 <parvej8pro@gmail.com>
2024-05-10 16:39:22 +0600

    express cors dotenv add

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+require('dotenv').config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const corsConfig = {
+  origin: '',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}
+app.use(cors(corsConfig))
+app.options("", cors(corsConfig))
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.MONGODB_TEN_USER}:${process.env.MONGODB_TEN_PASS}@cluster0.esbrpdb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 app.get('/',  (req, res) => {
   res.send('Server is running...')
